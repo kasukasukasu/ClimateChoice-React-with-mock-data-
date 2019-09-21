0e7f1b892708b170ab402b3a2ffb7772825c6dd3
kasukasukasu <kaarina.suominen@outlook.com>
2019-09-21 13:28:40 +0300

    mocked mongo data when server not running

--- src/components/Homepage/header.js
+++ src/components/Homepage/header.js
@@ -1,29 +1,45 @@
 import React, {Component} from 'react';
 import './header.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import "../SwipeGame/ItemDetailCards.css";
+
 
 class Header extends Component {
     render() {
         return(
             <div>
-                <div>
-                    <div className="container-fluid tausta-1">
-
-                    </div>
-                    <br/>
-                    <br/>
-                    <div className="container-fluid tausta-2">
-                            <h1 className="header-header">Climate Choice</h1>
-                        <h2> Apunasi kestävämpään arkeen</h2><br/>
-                        <h3 className="header-text">Climate Choice auttaa sinua tekemään kestävämpiä arjen ympäristövalintoja ja arvioi tekemisien valintojesi vaikutusta hiilijalanjälkeesi.</h3>
-                        <br/><br/>
-                       <HeaderButtons authentication={this.props.authentication}/>
-                    </div>
-                </div>
-                <div className="container-fluid tausta-2">
-                <footer className="footer">
-                        <p className="text-muted small mb-0">Copyright &copy; Team ClimateChoice 2018</p>
-                </footer>
-                </div>
+                        <div>
+                            <div>
+                                <div className="container-fluid tausta-2">
+                                    <h1 className="header-header">Tervetuloa tekemään arjen ilmastovalintoja.</h1><br/>
+                                    <h3 className="header-text">Peli antaa sinulle yksitellen arkipäivän haasteita.</h3><br/>
+                                    <h3 className="header-text">Valitse niistä kyllä tai ei.</h3><br/>
+                                    <Card className="infocard">
+                                        <CardContent>
+                                            <p>Valitsemalla kyllä, tehtävä siirtyy omaan listaasi, mikäli olet kirjautunut.</p><br/>
+                                            <p>Valitsemalla ei tehtävät siirtyvät omaan hylättyjen listaasi, mutta ei hätää sen voi aina palauttaa sieltä valittuihin tekoihin.</p><br/>
+                                            <p> Vihreä palkki näyttää tekemisien valintojesi vaikutuksen hiilijalanjälkeesi.</p><br/>
+                                        <p>Tehtävien merkityksestä ja vaikuttavuudesta saat lisätietoa painalla kortin oikeassa alareunassa olevaa nuolta.</p>
+                                        </CardContent>
+                                    </Card>
+                                    <div>
+                                        <div className="game">
+                                            <a className="button-header" href="/thechoice">Siirry tästä tekemään valintoja!</a>
+                                        </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="container-fluid tausta-2">
+                                <footer className="footer">
+                                    <p className="text-muted small mb-0">
+                                        Copyright &copy; Team ClimateChoice 2018
+                                        {/*<Image src={ClimateChoice} circle width="10" height="10" />*/}
+                                    </p>
+                                    {/*</div>*/}
+                                </footer>
+                            </div>
+                        </div>
             </div>
         );
     }
@@ -58,3 +74,24 @@ class HeaderButtons extends Component {
 }
 
 export default Header;
+
+
+/*                <div>
+                      <div className="container-fluid tausta-1">
+
+                      </div>
+                      <br/>
+                      <br/>
+                      <div className="container-fluid tausta-2">
+                              <h1 className="header-header">Climate Choice</h1>
+                          <h2> Apunasi kestävämpään arkeen</h2><br/>
+                          <h3 className="header-text">Climate Choice auttaa sinua tekemään kestävämpiä arjen ympäristövalintoja ja arvioi tekemisien valintojesi vaikutusta hiilijalanjälkeesi.</h3>
+                          <br/><br/>
+                         <HeaderButtons authentication={this.props.authentication}/>
+                      </div>
+                  </div>
+                  <div className="container-fluid tausta-2">
+                  <footer className="footer">
+                          <p className="text-muted small mb-0">Copyright &copy; Team ClimateChoice 2018</p>
+                  </footer>
+                  </div>*/
--- src/components/SwipeGame/TheSwipe.js
+++ src/components/SwipeGame/TheSwipe.js
@@ -14,7 +14,79 @@ class TheSwipe extends Component {
         this.state = {
             userid: this.props.user,
             authenticated: this.props.user,
-            tasks: [],
+            tasks: [
+                               {
+                               body: {
+                               content1: "Puut sitovat hiilidioksidia ja kompensoivat siten päästöjämme.",
+                               content2: "Puut sitovat hiiltä, viilentävät maapalloa ja rauhoittavat meitä ihmisiä. Yksi metsähehtaari sitoo 15 tonnia hiilidioksidia ja vapauttaa ilmakehään 10 tonnia happea joka vuosi. Jos istutat puun suojaamaan kotiasi auringonpaisteelta kesäisin ja tarjoamaan tuulensuojaa talvella, vähennät hiilidioksidipäästöjäsi ja mielesi rauhoittuu.",
+                               rating: "135"
+                               },
+                               _id: "5d7e29fb6927b7a29cfda449",
+                               title: "Istuta puu",
+                               createdAt: "2019-09-15T12:09:31.009Z",
+                               updatedAt: "2019-09-15T12:09:31.009Z",
+                               __v: 0
+                               },
+                               {
+                               body: {
+                               content1: "Kaupungissa asuminen on tiiviimpää ja palvelut ovat todennäköisesti sinua lähempänä.",
+                               content2: "Kaupungissa asuminen mahdollistaa kestävämmän elämäntavan omaksumisen helpommin. Tiiviissä kaupunkirakenteessa kaikki tarvittava on todennäköisesti lähellä ja liikkuminen vähenee. Kaupungeissa suuri joukko kotitalouksia käyttää samoja resursseja, joten niiden tuottamiseen kuluu vähemmän energiaa ja päästöt vähenevät.",
+                               rating: "4500"
+                               },
+                               _id: "5d7e2a616927b7a29cfda44a",
+                               title: "Muuta kaupunkiin",
+                               createdAt: "2019-09-15T12:11:13.122Z",
+                               updatedAt: "2019-09-15T12:11:13.122Z",
+                               __v: 0
+                               },
+                               {
+                               body: {
+                               content1: "Valitse metro auton sijaan siellä, missä se on mahdollista.",
+                               content2: "Täysi metrojunallinen voi poistaa teiltä yli 2 000 autoa. Jos vaihdat autosi julkisiin liikennevälineisiin edes kerran viikossa, saat aikaan jo suuren muutoksen.",
+                               rating: "810"
+                               },
+                               _id: "5d7e2a9e6927b7a29cfda44b",
+                               title: "Mene metrolla",
+                               createdAt: "2019-09-15T12:12:14.925Z",
+                               updatedAt: "2019-09-15T12:12:14.925Z",
+                               __v: 0
+                               },
+                               {
+                               body: {
+                               content1: "Käytä täytettävää juomapulloa vähentääksesi muovijätettä.",
+                               content2: "Jos tarvitset kyytiä tai voit sellaisen tarjota, netissä on useita sivustoja, joiden avulla kyydit ja niiden tarvitsijat löytävät toisensa. Ehdota kimppakyytejä myös esimerkiksi työmatkoillesi, lasten harrastuksiin kuskaamiseen tai vaikka kauppareissulle naapurin kanssa",
+                               rating: "10"
+                               },
+                               _id: "5d7e3d11913858c200878344",
+                               title: "Täytä juomapullosi",
+                               createdAt: "2019-09-15T13:30:57.187Z",
+                               updatedAt: "2019-09-15T13:30:57.187Z",
+                               __v: 0
+                               },
+                               {
+                               body: {
+                               content1: "Valitse mahdollisuuksien mukaan lähiruokaa ja poikkea lähimpään maatilamyymälään.",
+                               content2: "Tuoretavarat matkustavat keskimäärin 2 415 km tuotantoalueelta lautasellemme. Jopa 30% teillä kulkevista ajoneuvoista kuljettaa elintarvikkeita, joten ruuan saaminen koteihimme aiheuttaa suuret päästöt. Kannattaa siis kiinnittää huomiota siihen, kuinka pitkän matkan ruokasi tekee. Maatilatuotteita voi olla mahdollista ostaa suoraan tuottajilta tai voit liittyä lähiruokaa toimittavaan ruokarinkiin esimerkiksi facebookissa.",
+                               rating: "1500"
+                               },
+                               _id: "5d7e3d4f913858c200878345",
+                               title: "Osta lähellä tuotettua ruokaa",
+                               createdAt: "2019-09-15T13:31:59.415Z",
+                               updatedAt: "2019-09-15T13:31:59.415Z",
+                               __v: 0
+                               },
+                               {
+                               body: {
+                               content1: "Siirry paperisesta lehdestä verkkolehteen",
+                               content2: "Yhteen tonniin sanomalehtiä kuluu saman verran energiaa kuin eurooppalaiseen kolmen huoneen asuntoon vuodessa. Yhden ihmisen vuoden lehtien lukemisesta aiheutuu 270 kg hiilidioksidipäästöt, kun taas verkkouutisten lukemisessa päästöt ovat vain 5 kg vuodessa.",
+                               rating: "265"
+                               },
+                               _id: "5d7e3db3913858c200878346",
+                               title: "Lue lehtesi verkossa",
+                               createdAt: "2019-09-15T13:33:39.260Z",
+                               updatedAt: "2019-09-15T13:33:39.260Z",
+                               __v: 0
+                               }],
             index: 0,
             open: false,
             count: 0,
@@ -24,10 +96,10 @@ class TheSwipe extends Component {
     }
 
     componentDidMount() {
-        this.getAll();
+        //this.getAll();
     }
 
-    getAll() {
+    /*getAll() {
         fetchall(this.allFetched)
     }
 
@@ -39,7 +111,7 @@ class TheSwipe extends Component {
 
     getAllRelations(datatasks) {
         fetchallchoices(this.allChoices.bind(this, datatasks))
-    }
+    }*/
 
     allChoices(tasksdata, relationsdata) {
         var relations = relationsdata;
